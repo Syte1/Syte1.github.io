e2ab2d16c044507e958627f1657b69fbb9f133f9
Belal <belalk@live.ca>
2023-04-17 01:19:38 -0700

    Add project

---
+++ src/pages/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import {
+  AboutSection,
+  ArticlesSection,
+  ContactSection,
+  HeroSection,
+  InterestsSection,
+  Page,
+  ProjectsSection,
+  Seo,
+} from "gatsby-theme-portfolio-minimal";
+
+export default function IndexPage() {
+  return (
+    <>
+      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Page useSplashScreenAnimation>
+        <HeroSection sectionId="hero" />
+        <AboutSection sectionId="about" heading="About me" />
+        <InterestsSection sectionId="details" heading="Skills" />
+        <ProjectsSection sectionId="features" heading="Projects" />
+        <ContactSection sectionId="github" heading="Looking for a dev?" />
+      </Page>
+    </>
+  );
+}
